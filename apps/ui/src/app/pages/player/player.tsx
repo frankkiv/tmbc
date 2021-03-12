16be2bd39cf314117b4f3988c9555cb99bbb0627
Frank CH Lu <frank_ch_lu@trendmicro.com>
2021-03-12 16:52:21 +0800

    add the route param parsing

--- apps/ui/src/app/pages/player/player.tsx
+++ apps/ui/src/app/pages/player/player.tsx
@@ -1,14 +1,15 @@
 import React from 'react';
-
+import { useParams } from 'react-router-dom'
 import './player.module.scss';
 
 /* eslint-disable-next-line */
 export interface PlayerProps {}
 
 export function Player(props: PlayerProps) {
+  const { id } = useParams();
   return (
     <div>
-      <h1>Welcome to Player!</h1>
+      <h1>Welcome to Player {id} ! </h1>
     </div>
   );
 }
--- apps/ui/src/app/pages/team/team.tsx
+++ apps/ui/src/app/pages/team/team.tsx
@@ -1,14 +1,15 @@
 import React from 'react';
-
+import { useParams } from 'react-router-dom';
 import './team.module.scss';
 
 /* eslint-disable-next-line */
 export interface TeamProps {}
 
 export function Team(props: TeamProps) {
+  const { id } = useParams();
   return (
     <div>
-      <h1>Welcome to Team!</h1>
+      <h1>Welcome to Team {id}!</h1>
     </div>
   );
 }
