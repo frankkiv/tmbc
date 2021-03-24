c5846ae01f49092892543f95becdc386a608e086
Frank CH Lu <frank_ch_lu@trendmicro.com>
2021-03-24 15:19:40 +0800

    implement proxy.conf.json for testing

--- apps/ui/src/app/pages/player/player.tsx
+++ apps/ui/src/app/pages/player/player.tsx
@@ -1,15 +1,67 @@
-import React from 'react';
-import { useParams } from 'react-router-dom'
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
 import './player.module.scss';
-
+import axios from 'axios';
+import { Table, Tag, Space } from 'antd';
+import { mockData } from './mockData';
 /* eslint-disable-next-line */
 export interface PlayerProps {}
 
 export function Player(props: PlayerProps) {
   const { id } = useParams();
+  const [records, setRecords] = useState<any>([]);
+
+  useEffect(() => {
+    // axios
+    //   .get(`http://127.0.0.1:4200/api/Home/PlayerStats/8800`)
+    //   .then((res)=>setRecords(res.data));
+    setRecords(mockData);
+  }, []);
+
+  const columns = [
+    {
+      title: 'Name',
+      dataIndex: 'PlayerName',
+      key: 'PlayerName',
+    },
+    {
+      title: 'EventName',
+      dataIndex: 'EventName',
+      key: 'EventName',
+    },
+    {
+      title: 'GroupName',
+      dataIndex: 'GroupName',
+      key: 'GroupName',
+    },
+    {
+      title: 'Location',
+      dataIndex: 'location',
+      key: 'location',
+    },
+    {
+      title: 'Date',
+      dataIndex: 'date',
+      key: 'date',
+      render: (date: string) => {
+        const dateAndTime = date.split('T');
+        return dateAndTime[0].split('-').reverse().join('-');
+      },
+    },
+    {
+      title: 'Pts',
+      dataIndex: 'P',
+      key: 'Pts',
+    },
+  ];
+
   return (
     <div>
       <h1>Welcome to Player {id} ! </h1>
+      {/* {records.map((record) => {
+        return <h2>This is Game  {record?.GameId} ! </h2>;
+      })} */}
+      <Table dataSource={records} columns={columns} />;
     </div>
   );
 }
